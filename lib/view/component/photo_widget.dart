96dc14ffba973ef257cdd5b1ee37b204e0811d2f
LeeGeonMin <74194550+Leegeonmin@users.noreply.github.com>
2022-11-29 11:24:19 +0900

    feat: photo model 분리

---
+++ lib/view/component/photo_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_photoapp/model/photo.dart';
+
+class PhotoWidget extends StatelessWidget {
+  final Photo photo;
+  const PhotoWidget({super.key, required this.photo});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(20)),
+            image: DecorationImage(
+                fit: BoxFit.cover, image: NetworkImage(photo.previewURL))));
+  }
+}
