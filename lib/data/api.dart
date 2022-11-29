165eb526ad65a072d33b879184c6380a33c9b403
LeeGeonMin <74194550+Leegeonmin@users.noreply.github.com>
2022-11-29 11:48:48 +0900

    feat: api url, key 고유 값으로 분리

--- lib/data/api.dart
+++ lib/data/api.dart
@@ -2,10 +2,14 @@ import 'package:flutter_photoapp/model/photo.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-Future<List<Photo>> getPhotos(query) async {
-  final response = await http.get(Uri.parse(
-      'https://pixabay.com/api/?key=${query}&q=yellow+flowers&image_type=photo'));
-  Map<String, dynamic> decodeJson = jsonDecode(response.body);
-  Iterable items = decodeJson['hits'];
-  return items.map((e) => Photo.fromJson(e)).toList();
+class PixabayApi {
+  final baseUrl = 'https://pixabay.com/api/';
+  final apiKey = '31645494-99849d2438fe17bdb6f35e761';
+  Future<List<Photo>> getPhotos(query) async {
+    final response = await http
+        .get(Uri.parse('$baseUrl?key=$apiKey&q=$query&image_type=photo'));
+    Map<String, dynamic> decodeJson = jsonDecode(response.body);
+    Iterable items = decodeJson['hits'];
+    return items.map((e) => Photo.fromJson(e)).toList();
+  }
 }
