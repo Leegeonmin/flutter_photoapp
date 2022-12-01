5c3bcbbc292ddcf1b4499b27bc1bc7d9dcfeb380
dlrjsals97 <kmlee97s@gmail.com>
2022-12-02 00:29:51 +0900

    test: mokito를 활용한 pixabay_api test

--- lib/data/pixabay_api.dart
+++ lib/data/pixabay_api.dart
@@ -4,11 +4,13 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class PixabayApi implements PhotoApiRepository {
-  final baseUrl = 'https://pixabay.com/api/';
-  final apiKey = '31645494-99849d2438fe17bdb6f35e761';
+  static const baseUrl = 'https://pixabay.com/api/';
+  static const apiKey = '31645494-99849d2438fe17bdb6f35e761';
   @override
-  Future<List<Photo>> fetch(query) async {
-    final response = await http
+  Future<List<Photo>> fetch(query, {http.Client? client}) async {
+    client ??= http.Client();
+
+    final response = await client
         .get(Uri.parse('$baseUrl?key=$apiKey&q=$query&image_type=photo'));
     Map<String, dynamic> decodeJson = jsonDecode(response.body);
     Iterable items = decodeJson['hits'];
