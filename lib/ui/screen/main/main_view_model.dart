88c44ab2e296370b48bb1ec840de32e50896d0ca
dlrjsals97 <kmlee97s@gmail.com>
2022-12-05 00:50:11 +0900

    refactor: viewModel Provider로 변환, stream 삭제

--- lib/ui/screen/main/main_view_model.dart
+++ lib/ui/screen/main/main_view_model.dart
@@ -1,16 +1,21 @@
+import 'dart:collection';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_photoapp/data/photo_api_repository.dart';
 import 'package:flutter_photoapp/model/photo.dart';
 import 'dart:async';
 
-class MainViewModel {
+class MainViewModel with ChangeNotifier {
   final PhotoApiRepository repository;
 
-  final _photoStreamController = StreamController<List<Photo>>()..add([]);
-  Stream<List<Photo>> get photoStream => _photoStreamController.stream;
+  List<Photo> _photoList = [];
+// error throwing type
+  UnmodifiableListView<Photo> get photoList => UnmodifiableListView(_photoList);
 
   Future<void> fetch(String query) async {
     final result = await repository.fetch(query);
-    _photoStreamController.add(result);
+    _photoList = result;
+    notifyListeners();
   }
 
   MainViewModel(this.repository);
