26cb24884bc42adadd9374c749a1a8dc9b396558
dlrjsals97 <kmlee97s@gmail.com>
2022-12-07 16:13:31 +0900

    feat: viewModel에서 state 관리 객체 분리

---
+++ lib/presentation/main/home_state.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_photoapp/domain/model/photo.dart';
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'home_state.freezed.dart';
+
+@freezed
+class HomeState with _$HomeState {
+  factory HomeState(
+    List<Photo> photos,
+    bool isLoading,
+  ) = _HomeState;
+}
