5438145e5236325be73f06a4adf3486c055545fe
dlrjsals97 <kmlee97s@gmail.com>
2022-12-08 23:58:21 +0900

    feat: viewModel에서 business logic을 use case로 분리

---
+++ lib/domain/use_case/get_photos_use_case.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_photoapp/data/data_source/result.dart';
+import 'package:flutter_photoapp/domain/model/photo.dart';
+import 'package:flutter_photoapp/domain/repository/photo_api_repository.dart';
+
+class GetPhotosUseCase {
+  final PhotoApiRepository repository;
+
+  GetPhotosUseCase(this.repository);
+
+  Future<Result<List<Photo>>> execute(String query) async {
+    final Result<List<Photo>> result = await repository.fetch(query);
+    return result.when(success: (photos) {
+      return Result.success(photos.sublist(0, 3));
+    }, error: (message) {
+      return Result.error(message);
+    });
+  }
+}
